16fc0593a39a167483e78258b64e40a55d753767
mkamri <mail@mikamarie.com>
2020-03-21 11:48:56 -0400

    Add animation to museums section on desktop

--- js/museums.js
+++ js/museums.js
@@ -4,12 +4,20 @@ const daiSection = document.querySelector('.dai');
 const airforceSection = document.querySelector('.airforce');
 const boonshoftSection = document.querySelector('.boonshoft');
 const modalCloser = document.getElementById('js-close-modal');
+const modalContent = document.getElementById('modal-content');
 
 // Fade to black
 const fadeToBlack = function() {
-  sectionBackground.style.zIndex = `998`;
-  sectionBackground.style.backgroundPosition = `0 0`;
+	sectionBackground.style.zIndex = `998`;
+	sectionBackground.style.backgroundPosition = `0 0`;
+	setTimeout(() => (modalContent.style.opacity = '1'), 600);
 };
+
+document.addEventListener('click', function(e) {
+	if (e.target == daiSection) {
+		alert('DAI was clicked!');
+	}
+});
 
 daiSection.addEventListener('click', fadeToBlack);
 airforceSection.addEventListener('click', fadeToBlack);
@@ -17,8 +25,9 @@ boonshoftSection.addEventListener('click', fadeToBlack);
 
 // Close Modal
 const closeModal = function() {
-  sectionBackground.style.backgroundPosition = `100% 0`;
-  setTimeout(() => sectionBackground.style.zIndex = 0, 500);
-}
+	modalContent.style.opacity = '0';
+	setTimeout(() => (sectionBackground.style.backgroundPosition = '100% 0'), 400);
+	setTimeout(() => (sectionBackground.style.zIndex = 0), 800);
+};
 
 modalCloser.addEventListener('click', closeModal);
