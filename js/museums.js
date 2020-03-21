831842282783a47a58efc9ef35dae33b7987d7cb
mkamri <mail@mikamarie.com>
2020-03-21 12:22:07 -0400

    Change text based on which section is clicked

--- js/museums.js
+++ js/museums.js
@@ -5,6 +5,9 @@ const airforceSection = document.querySelector('.airforce');
 const boonshoftSection = document.querySelector('.boonshoft');
 const modalCloser = document.getElementById('js-close-modal');
 const modalContent = document.getElementById('modal-content');
+const modalHeading = document.getElementById('modal-heading');
+const modalParagraph = document.getElementById('modal-paragraph');
+const museumSiteLink = document.getElementById('museum-site-link');
 
 // Fade to black
 const fadeToBlack = function() {
@@ -13,9 +16,20 @@ const fadeToBlack = function() {
 	setTimeout(() => (modalContent.style.opacity = '1'), 600);
 };
 
+// Show different text based on what section was clicked
 document.addEventListener('click', function(e) {
 	if (e.target == daiSection) {
-		alert('DAI was clicked!');
+		modalHeading.textContent = 'Dayton Art Institute';
+		modalParagraph.textContent = `The Dayton Art Institute, founded in 1919, is one of the most well-known art institutes in the region. Featuring special exhibits from around the world, as well as a permanent collection spanning 5,000 years of art history, the DAI is a must-see for any art enthusiast. It also plays host to concerts as well as signature events such as the annual Art Ball and Oktoberfest.`;
+		museumSiteLink.href = 'http://www.daytonartinstitute.org/';
+	} else if (e.target == airforceSection) {
+		modalHeading.textContent = 'National Museum of the US Air Force';
+		modalParagraph.textContent = `Dayton's National Museum of the US Air Force is the oldest and largest millitary aviation museum in the world.Founded in 1923, the Air Force Museum has been a favorite for Daytonians and visitors alike for generations. The museum contains many rare aircrafts and historical artifacts, including the Apollo 15 which orbited the Moon in 1971, as well as the Boeing B-20 that dropped the Fat Man atomic bomb during the last days of WWII.`;
+		museumSiteLink.href = 'https://www.nationalmuseum.af.mil/';
+	} else if (e.target == boonshoftSection) {
+		modalHeading.textContent = 'Boonshoft Museum of Discovery';
+		modalParagraph.textContent = `The Boonshoft, a museum of science and technology, teaches natural history and discovery for all ages. The museum houses Apollo Observatory, a planetarium with daily light shows, and an aquarium. The museum also is home to Nesiur the Mummy, fossils, animal skeletons, and other archaeological artifacts. The museum also hosts special events each year, such as GeekFest, Eureka!, and other large-scale activities in support of Biology, Nanotechnology, and Chemistry.`;
+		museumSiteLink.href = 'https://www.boonshoftmuseum.org/';
 	}
 });
 
