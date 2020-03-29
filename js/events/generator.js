fc7da592bbc3096bff0ab0f25c1486df8c2dcedd
mkamri <mail@mikamarie.com>
2020-03-29 14:23:48 -0400

    Shorten long titles and add elipses

--- js/events/generator.js
+++ js/events/generator.js
@@ -38,6 +38,34 @@ const musicEvents = [
 	}
 ];
 
+const sportsEvents = [
+	{
+		title: '2020 CoronaBusters Virtual 19K Run Walk',
+		date: 'Fri, May 1, 2020',
+		location: 'Dayton',
+		image: '/css/img/events/event-img/coronabusters.jpg'
+	},
+	{
+		title: '2020 Remember the Fallen Virtual 21K Run Walk',
+		date: 'Fri, May 15',
+		location: 'Dayton',
+		image: '/css/img/events/event-img/remember'
+	},
+	{
+		title: '2020 Anything is PAW-sible 5k Run Walk',
+		date: 'tbd',
+		location: 'Dayton',
+		image: '/css/img/events/event-img/pawsible'
+	},
+	{
+		title:
+			'Royal Boxing Promotions Presents RED CARPET FIGHT NIGHT. An action-packed night of professinoal boxing. Come SUPPORT, CHEER, and HAVE FUN.',
+		date: '',
+		location: '',
+		image: '/css/img/events/event-img/'
+	}
+];
+
 // Define Variables
 const eventList = document.getElementById('app-list');
 
@@ -46,6 +74,16 @@ musicEvents.forEach(addEvent);
 
 function addEvent(event) {
 	let listItem = document.createElement('li');
+
+	// Determine if the title will get cut short and need elipses
+	let elipses;
+	if (event.title.length > 35) {
+		elipses = '...';
+	} else {
+		elipses = '';
+	}
+
+	// Insert inner HTML
 	listItem.className = `list-group-item d-flex align-items-center`;
 	listItem.innerHTML = `
 			<div class="event-img" style="background-image: url('${event.image}')"></div>
@@ -53,7 +91,7 @@ function addEvent(event) {
 				<span class="text-primary mr-3">${event.date}</span>
 				<br>
 				<span class="text-danger d-none d-sm-inline-block">${event.location}</span>
-				<h2 class="event-title h4 text-light">${event.title}</h2>
+				<h2 class="event-title h4 text-light">${event.title.slice(0, 35)}${elipses}</h2>
 				<a href="#" class="btn btn-danger btn-sm">Visit Site</a>
 			</div>
   `;
